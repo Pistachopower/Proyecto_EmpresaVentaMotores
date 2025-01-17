d18a356d2b653b100d9345017795146e64380b0b
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-01-17 13:22:41 +0100

    se arregla error en el setting contenttypes

--- AplicacionWeb/mysite/settings.py
+++ AplicacionWeb/mysite/settings.py
@@ -52,8 +52,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'django_bootstrap5',
     'django_bootstrap_icons',
-    'django.contrib.auth',  
-    'django.contrib.contenttypes', 
+    
 ]
 
 MIDDLEWARE = [
@@ -66,7 +65,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',    
-    'django.contrib.auth.middleware.AuthenticationMiddleware', 
+  
 ]
 
 ROOT_URLCONF = 'mysite.urls'
