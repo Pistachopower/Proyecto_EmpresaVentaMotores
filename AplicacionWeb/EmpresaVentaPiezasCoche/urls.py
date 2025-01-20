c71623606b4f44207e03ceb48d2cc22f1900f366
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-01-20 23:44:22 +0100

    implementacion de contraseñas

--- AplicacionWeb/EmpresaVentaPiezasCoche/urls.py
+++ AplicacionWeb/EmpresaVentaPiezasCoche/urls.py
@@ -3,6 +3,10 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+#recuperacion passw
+from django.urls import path 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('clientes/listar', views.listar_clientes, name='listar_clientes'),
@@ -50,4 +54,24 @@ urlpatterns = [
 
     #sesiones
     path('registrar',views.registrar_usuario,name='registrar_usuario'),
+    
+    
+
+    # Ruta para solicitar el restablecimiento de contraseña
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html'), name='password_reset'),
+    
+    # Ruta para notificar que se ha enviado el correo
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    
+    # Ruta para ingresar la nueva contraseña (incluye el token)
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    
+    # Ruta para notificar que la contraseña se ha cambiado correctamente
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+
+
 ]
