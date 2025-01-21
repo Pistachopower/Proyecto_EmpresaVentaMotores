9f857049ed65f683ef49c7d907c0c7c4c918f5bb
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-01-21 12:16:42 +0100

    otros cambios

--- AplicacionWeb/EmpresaVentaPiezasCoche/api_urls.py
+++ AplicacionWeb/EmpresaVentaPiezasCoche/api_urls.py
@@ -6,7 +6,7 @@ from .api_views import listar_clientes
 urlpatterns = [
     #http://127.0.0.1:8000/api/v1/clientes/
     path('clientes/',listar_clientes, name='listar_clientes'),     #Es una buena práctica incluir una barra (/) al final de las URLs para que Django pueda manejarlas correctamente.
-
+    
     
 ]
 
